289774cc556346cd1a38c38b150ada594f073f4b
ashwinimore98 <109162315+ashwinimore98@users.noreply.github.com>
2022-07-13 17:09:11 +0530

    Add files via upload

---
+++ switch case.sh
@@ -0,0 +1,13 @@
+claer
+echo "Enter the number to view the day"
+read num
+case ${num} in
+0) echo "sun";;
+1) echo "mun";;
+3) echo "tue";;
+4) echo "wed";;
+5) echo "thur";;
+6) echo "fri";;
+7) echo "sat";;
+esac
+
